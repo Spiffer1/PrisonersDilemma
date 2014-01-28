98f11d5866e63bd3a26f4a84947f8d98b5c52145
Sean Fottrell <sfottrell@att.net>
2014-01-27 22:56:47 -0800

    Use reflection to dynamically load player classes.

--- PDDriver.java
+++ PDDriver.java
@@ -1,8 +1,122 @@
+import java.io.File;
+import java.io.FilenameFilter;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.JavaClass;
+
 public class PDDriver
 {  
+    /* 
+     * We look in the current directory for a directory named 'plugins'.  If that directory
+     * contains any .class files, we dynamically load them into ArrayList playerList. We then
+     * test them to see if they implement the PDPlayer inteface. If not, we remove them from
+     * the playerList.
+     */
     public static void main(String[] args)
     {
-        PDTournament game = new PDTournament();
-        game.run();
+        // Get the current directory
+        File currentDir = new File(System.getProperty("user.dir"));
+        System.out.println("The current directory is: " + currentDir.getAbsolutePath());
+
+        // Look for the "plugins" directory and get a list of the class files
+        File pluginsDir = new File(currentDir + "/" + "plugins");
+        ArrayList<Class<?>> playerList = new ArrayList<Class<?>>();
+        if (pluginsDir.exists() && pluginsDir.isDirectory()) {
+            // First, get a list of all the Java class files
+            File[] classFileList = getClassFiles(pluginsDir);
+
+            // Next, from the list of class files, create a list of Class objects
+            playerList = getClasses(pluginsDir, classFileList);
+        } else {
+            System.err.println(String.format("The directory '%s' is missing.", pluginsDir.getAbsolutePath()));
+        }
+        // Remove player classes that do not implement PDPlayer Interface. Do this by constructing
+        // an array with the indices of the classes that need removing.
+        System.out.println("Array has " + playerList.size() + " classes");
+        int[] removals = new int[playerList.size()];
+        int numToRemove = 0;
+        for (int i = 0; i < playerList.size(); i++)
+        {
+            if (PDPlayer.class.isAssignableFrom(playerList.get(i)) == false)
+            {
+                removals[numToRemove] = i;
+                numToRemove++;
+            }
+        }
+        System.out.println("Remove this many: " + numToRemove);
+        for (int i = numToRemove - 1; i >= 0; i--)
+        {
+            playerList.remove(i);
+        }
+        System.out.println("Array now has " + playerList.size() + " classes");
+
+        PDTournament tourney = new PDTournament();
+        tourney.run(playerList);
+    }
+
+    /*
+     * Returns an ArrayList of Java Class objects that exist in the directory "pluginsDir"
+     */
+    private static ArrayList<Class<?>> getClasses(File pluginsDir, File[] classFileList) {
+        ArrayList<Class<?>> result = null;
+
+        try {
+            URL[] urls = { pluginsDir.toURI().toURL() };
+            URLClassLoader classLoader = new URLClassLoader(urls, PDDriver.class.getClassLoader());
+
+            result = new ArrayList<Class<?>>();
+            for (File classFile : classFileList) {
+                ClassParser classParser = new ClassParser(classFile.getAbsolutePath());
+                JavaClass javaClass = classParser.parse();
+                String className = javaClass.getClassName();
+                System.out.println(String.format("The class name is '%s'.", className));
+                Class<?> theClass = classLoader.loadClass(className);
+                result.add(theClass);
+            }
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /*
+     * Returns the list of files in the 'pluginsDir' that end with ".class"
+     */
+    private static File[] getClassFiles(File pluginsDir) {
+        File[] result = null;
+
+        // create new filename filter for ".class" files
+        FilenameFilter fileNameFilter = new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    boolean result = false;
+                    if (name.lastIndexOf('.') > 0) {
+                        // get last index for '.' char
+                        int lastIndex = name.lastIndexOf('.');
+
+                        // get extension
+                        String str = name.substring(lastIndex);
+
+                        // match path name extension
+                        if (str.equals(".class")) {
+                            result = true;
+                        }
+                    }
+                    return result;
+                }
+            };
+
+        //
+        // Use the FilenameFilter we created "in-line" above to get a list of all files
+        // ending with ".class".
+        //
+        result = pluginsDir.listFiles(fileNameFilter);
+
+        return result;
     }
 }
--- PDTournament.java
+++ PDTournament.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.lang.reflect.Method;
+
 public class PDTournament
 {
     final int NUMPLAYERS = 4;
@@ -5,7 +8,7 @@ public class PDTournament
     final int PUNISHMENT = 0;
     final int TEMPTATION = 2;
     final int SUCKER = -1;
-    final int NUMROUNDS = 200;  // each round consists of two plays
+    final int NUMROUNDS = 3;  // each round consists of two plays
     private PlayerRecord[] player;
 
     public PDTournament()
@@ -17,70 +20,78 @@ public class PDTournament
         }
     }
 
-    public void run()
+    public void run(ArrayList<Class<?>> playerList)
     {
-        for (int p0 = 0; p0 < player.length; p0++)
+        for (int p0 = 0; p0 < player.length - 1; p0++)
         {
             System.out.println("p0 = " + p0);
             for (int p1 = p0 + 1; p1 < player.length; p1++)
             {
                 // Below is the main algorithm for game play
-                PDPlayer firstPlayer = new TitForTat();  // default player type is TitForTat
-                PDPlayer secondPlayer = new TitForTat();
-                //Enter actual type for each of the players below:
-                if (p0 == 0)
-                    firstPlayer = new Generous();
-                if (p0 == 1)
-                    firstPlayer = new Unhelpful();
-                if (p0 == 2)
-                    firstPlayer = new TitForTat();
-                if (p0 == 3)
-                    firstPlayer = new BigGrudge();
-                if (p1 == 0)
-                    secondPlayer = new Generous();
-                if (p1 == 1)
-                    secondPlayer = new Unhelpful();
-                if (p1 == 2)
-                    secondPlayer = new TitForTat();
-                if (p1 == 3)
-                    secondPlayer = new BigGrudge();
-
-                String[] history = new String[NUMROUNDS * 2];    
-                String p0LastMove = "";
-                String p1LastMove = "";
-                for (int round = 0; round < NUMROUNDS; round++)
+                try
                 {
-                    String p0Move = firstPlayer.chooseCorD(p1LastMove);
-                    String p1Move = secondPlayer.chooseCorD(p0LastMove);
-                    history[round] = p0Move + p1Move;
-                    p0LastMove = p0Move;
-                    p1LastMove = p1Move;
-                    if (history[round].equals("cc"))
+                    Object firstPlayer = playerList.get(p0).newInstance();
+                    Object secondPlayer = playerList.get(p1).newInstance();
+                    String[] history = new String[NUMROUNDS * 2];    
+                    String p0LastMove = "";
+                    String p1LastMove = "";
+                    for (int round = 0; round < NUMROUNDS; round++)
                     {
-                        player[p0].addToScore(REWARD);
-                        player[p1].addToScore(REWARD);
-                    }
-                    if (history[round].equals("cd"))
-                    {
-                        player[p0].addToScore(SUCKER);
-                        player[p1].addToScore(TEMPTATION);
-                    }
-                    if (history[round].equals("dc"))
-                    {
-                        player[p0].addToScore(TEMPTATION);
-                        player[p1].addToScore(SUCKER);
-                    }
-                    if (history[round].equals("dd"))
-                    {
-                        player[p0].addToScore(PUNISHMENT);
-                        player[p1].addToScore(PUNISHMENT);
+                        String p0Move = invokeMethod(playerList.get(p0), "chooseCorD", firstPlayer, p1LastMove);
+                        // firstPlayer.chooseCorD(p1LastMove);
+                        String p1Move = invokeMethod(playerList.get(p1), "chooseCorD", secondPlayer, p0LastMove);
+                        //secondPlayer.chooseCorD(p0LastMove);
+                        history[round] = p0Move + p1Move;
+                        p0LastMove = p0Move;
+                        p1LastMove = p1Move;
+                        if (history[round].equals("cc"))
+                        {
+                            player[p0].addToScore(REWARD);
+                            player[p1].addToScore(REWARD);
+                        }
+                        if (history[round].equals("cd"))
+                        {
+                            player[p0].addToScore(SUCKER);
+                            player[p1].addToScore(TEMPTATION);
+                        }
+                        if (history[round].equals("dc"))
+                        {
+                            player[p0].addToScore(TEMPTATION);
+                            player[p1].addToScore(SUCKER);
+                        }
+                        if (history[round].equals("dd"))
+                        {
+                            player[p0].addToScore(PUNISHMENT);
+                            player[p1].addToScore(PUNISHMENT);
+                        }
                     }
                 }
+                catch (Exception e)
+                {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
                 for (int playerNum = 0; playerNum < player.length; playerNum++)
                 {
                     System.out.println("Player " + playerNum + ": " + player[playerNum].getScore());
                 }
             }
         }
+    }
+    /*
+     * Invokes the method 'theMethod' from the class 'theClass' in Object instance
+     */
+    private static String invokeMethod(Class<?> theClass, String theMethod, Object player, String opponentsLastMove) {
+        String result = "Method failed.";
+        try {
+            Method chooseCorDMethod = theClass.getMethod(theMethod, String.class);
+            result = (String) chooseCorDMethod.invoke(player, opponentsLastMove);
+            //System.out.println(String.format("The result of calling 'getName()' on '%s' is '%s'.", theClass.getName(), result));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return result;
     }
 }
